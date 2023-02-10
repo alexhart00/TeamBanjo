cd552f3fa02baa383bdd77e8dc3644b7e1a16971
Alexander Hart <alexhart3000@gmail.com>
2023-02-09 18:55:36 -0700

    Item Container Detail View on Random

--- Socksandfrocks/src/routes/Random/RandomPage.server.jsx
+++ Socksandfrocks/src/routes/Random/RandomPage.server.jsx
@@ -1,12 +1,13 @@
 import NavBar from "../Shared/NavBar.server";
 import ItemDetails from "../Shared/ItemDetails.server";
+import ItemContainerDetail from "../Shared/ItemContainerDetail";
 
 export default function RandomPage(){
     return(
         <div>
             <NavBar/>
             RandomPage
-            <ItemDetails/>
+            <ItemContainerDetail/>
         </div>
     );
 }
