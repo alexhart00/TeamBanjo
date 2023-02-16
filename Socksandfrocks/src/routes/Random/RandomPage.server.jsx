78c3393b9385b36d8ebe95b820c8fd6c471fa030
Alexander Hart <alexhart3000@gmail.com>
2023-02-15 21:14:51 -0700

    Work on RandomPickout, ProjectPlan update week 5+6

--- Socksandfrocks/src/routes/Random/RandomPage.server.jsx
+++ Socksandfrocks/src/routes/Random/RandomPage.server.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../Shared/NavBar.server";
 import ItemDetails from "../Shared/ItemDetails.server";
 import ItemContainerDetail from "../Shared/ItemContainerDetail";
+import RandomRigoutPickoutWindow from "../Shared/RandomRigoutPickoutWindow";
 
 export default function RandomPage(){
     return(
