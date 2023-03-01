cbce7c16dce3d30b88ac050456a0b2d461d689aa
Emma Ogle <89550274+emmagesena@users.noreply.github.com>
2023-03-01 12:38:21 -0700

    add carousel packages and implement carousel component

--- Socksandfrocks/src/routes/Manual/ManualPage.jsx
+++ Socksandfrocks/src/routes/Manual/ManualPage.jsx
@@ -1,6 +1,19 @@
-import NavBar from "../Shared/NavBar.client";
+import ItemCarousel from "../Shared/Carousel.client";
+
 
 export default function ManualPage({clothingData, showComponent}){
+
+    const topImages= [
+    "https://img1.shopcider.com/product/1654572280000-bf8G63.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1", 
+    "https://img1.shopcider.com/product/1652527482000-6YE8Dr.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+    "https://img1.shopcider.com/product/1666064255000-Z3YXaD.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1"
+];
+
+    const bottomImages = [
+    "https://img1.shopcider.com/product/1666062089000-Ffr8Tm.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+    "https://img1.shopcider.com/product/1670349085000-4eaHSB.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+    "https://img1.shopcider.com/product/1664874940000-8Kjc6J.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1"
+    ];
 
     var bottomArray = JSON.parse(clothingData[0]);
     var topArray = JSON.parse(clothingData[1]);
@@ -9,10 +22,11 @@ export default function ManualPage({clothingData, showComponent}){
     if(showComponent == true){
         return(
             <div>
-                 ManualPage
-                 {/* show data demo -- can delete */}
-                 <p>{bottomArray[0].itemName}</p>
+                 <p>Manual Page</p>
+                 <ItemCarousel imgUrls={topImages}></ItemCarousel>
+                 <ItemCarousel imgUrls={bottomImages}></ItemCarousel>
             </div>
+            
         );
     }
     else{
---
+++ Socksandfrocks/src/routes/Shared/Carousel.client.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Carousel from '@somedaycode/react-carousel';
+import styled from 'styled-components';
+
+export default function ItemCarousel({imgUrls}){
+
+    const options = {
+        carouselWidth: 200,
+        itemsToShow: 0,
+        duration: 0.5,
+        timing: 'ease-in-out',
+        arrowSize: 20,
+      };
+
+      const Item = styled.div`
+      margin: 0 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 40px;
+      background: linear-gradient(to right, #00FFFF, #A020F0);
+      border-radius: 10px;
+      width: 190px; // 200px - margin(5px + 5px)
+      height: 200px;
+      `;
+      const items = imgUrls.map ((card) => (
+        <Item><img src={card}></img></Item>
+      ));
+
+      return(
+        <div>
+            <Carousel {...options}>{items}</Carousel>
+        </div>
+      )
+
+}
