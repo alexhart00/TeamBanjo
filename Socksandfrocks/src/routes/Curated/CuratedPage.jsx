3b255bf9cea68dcab3c8078335c0f004274ee47d
Alex Hart <alexhart3000@gmail.com>
2023-03-02 16:05:00 -0700

    Curated Update part 1

--- Socksandfrocks/src/routes/Curated/CuratedPage.jsx
+++ Socksandfrocks/src/routes/Curated/CuratedPage.jsx
@@ -1,17 +1,35 @@
+import React, { useState } from "react";
 import NavBar from "../Shared/NavBar.client";
+import CuratedPageRigout from "../Shared/CuratedPageRigout";
+import ItemContainerDetail from "../Shared/ItemContainerDetail";
 
 export default function CuratedPage({clothingData, showComponent}){
+    
+    const topImages= [
+        "https://img1.shopcider.com/product/1654572280000-bf8G63.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1", 
+        "https://img1.shopcider.com/product/1652527482000-6YE8Dr.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+        "https://img1.shopcider.com/product/1666064255000-Z3YXaD.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1"
+    ];
+    
+        const bottomImages = [
+        "https://img1.shopcider.com/product/1666062089000-Ffr8Tm.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+        "https://img1.shopcider.com/product/1670349085000-4eaHSB.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
+        "https://img1.shopcider.com/product/1664874940000-8Kjc6J.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1"
+    ];
 
     var bottomArray = JSON.parse(clothingData[0]);
     var topArray = JSON.parse(clothingData[1]);
     var outfitArray = JSON.parse(clothingData[2]);
 
+    var selectedItem = bottomArray[0];
+
     if(showComponent == true){
         return(
             <div>
                 CuratedPage
-                {/* show data demo -- can delete */}
-                <p>{bottomArray[0].itemName}</p>
+                <CuratedPageRigout URl={topImages}/>
+                <ItemContainerDetail item={selectedItem}/>
+                <ItemContainerDetail item={selectedItem}/>
             </div>
         );
     }
