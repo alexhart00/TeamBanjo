3d0cab59f89fe611983865ae530d21b3482f8694
Emma Ogle <89550274+emmagesena@users.noreply.github.com>
2023-03-03 13:49:57 -0700

    grab urls from objects for caroseul

--- Socksandfrocks/src/routes/Manual/ManualPage.jsx
+++ Socksandfrocks/src/routes/Manual/ManualPage.jsx
@@ -5,22 +5,12 @@ import ItemContainerDetail from "../Shared/ItemContainerDetail";
 
 export default function ManualPage({clothingData, showComponent}){
 
-    const topImages= [
-    "https://img1.shopcider.com/product/1654572280000-bf8G63.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1", 
-    "https://img1.shopcider.com/product/1652527482000-6YE8Dr.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
-    "https://img1.shopcider.com/product/1666064255000-Z3YXaD.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
-    "https://cdn.shopify.com/s/files/1/0057/8384/6985/products/IMG_22852_1024x1024@2x.heic?v=1677807293"
-    ];
-
-    const bottomImages = [
-    "https://img1.shopcider.com/product/1666062089000-Ffr8Tm.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
-    "https://img1.shopcider.com/product/1670349085000-4eaHSB.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1",
-    "https://img1.shopcider.com/product/1664874940000-8Kjc6J.jpg?x-oss-process=image/resize,w_700,m_lfit/quality,Q_80/interlace,1"
-    ];
-
     var bottomArray = JSON.parse(clothingData[0]);
     var topArray = JSON.parse(clothingData[1]);
     var outfitArray = JSON.parse(clothingData[2]);
+
+    var topImages = getImages(topArray);
+    var bottomImages = getImages(bottomArray);
 
     var selectedItem = bottomArray[0];
 
@@ -49,4 +39,14 @@ export default function ManualPage({clothingData, showComponent}){
             <div></div>
         );
     }
+}
+
+function getImages(array){
+    var imageArray = [];
+
+    for(let i = 0; i < array.length; i++){
+        imageArray.push(array[i].photoUrl);
+    }
+
+    return imageArray;
 }
