e15c3a8886e2d7d4dc2a2a91c68b7fe932f0ea2b
Alexander Hart <alexhart3000@gmail.com>
2023-03-22 18:46:44 -0600

    Home Page Update P2

--- Socksandfrocks/src/routes/Shared/MainHomePage.jsx
+++ Socksandfrocks/src/routes/Shared/MainHomePage.jsx
@@ -1,15 +1,23 @@
 export default function MainHomePage({}) {
     return(
       <div>
-        <div class="grid grid-cols-2 flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
-              <p>HomePage</p>
+        <div class=" w-6/12 items-center p-8 centerDiv text-purple-900">
+              <h1 class="mb-4 text-4xl">Welcome to SocksandFrocks.com</h1>
+              <p> “all text is placeholder” Here are some exciting applications to make your shopping experience a blast.</p>
         </div>
-        <div class="grid grid-cols-2 flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
-              <p>HomePage</p>
+        <div><p>&nbsp;</p></div>
+        <div class="flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
+              <h1 class="mb-4 text-4xl">Curated Rigout Pickout</h1>
         </div>
-        <div class="grid grid-cols-2 flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
-              <p>HomePage</p>
+        <div><p>&nbsp;</p></div>
+        <div class="flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
+              <h1 class="mb-4 text-4xl">Manual Rigout Pickout</h1>
         </div>
+        <div><p>&nbsp;</p></div>
+        <div class="flex flex-wrap w-6/12 items-center p-8 bg-gray-100 border-gray-200 border-8 content-center centerDiv shadow-lg">
+              <h1 class="mb-4 text-4xl">Random Rigout Pickout</h1>
+        </div>
+        <div><p>&nbsp;</p></div>
       </div>
     );
   }
