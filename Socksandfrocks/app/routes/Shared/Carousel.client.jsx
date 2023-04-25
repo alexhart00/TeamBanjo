155a4f51bec2b130a5fafa4a66a29c0cbe2909eb
Alexander Hart <alexhart3000@gmail.com>
2023-04-25 16:05:41 -0600

    Carousel client update

--- Socksandfrocks/app/routes/Shared/Carousel.client.jsx
+++ Socksandfrocks/app/routes/Shared/Carousel.client.jsx
@@ -1,108 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
+import Carousel from '@somedaycode/react-carousel';
 import styled from 'styled-components';
-import Arrow from '@somedaycode/react-carousel/dist/Arrow';
 
-const Carousel = ({
-  updateLeftFlag,
-  leftFlagValue,
-  updateRightFlag,
-  rightFlagValue,
-  children,  
-  ...options
-}) => {
-  const {
-    itemsToShow,
-    duration,
-    timing,
-    arrowSize,
-    carouselWidth
-  } = options;
-  const [position, setPosition] = useState(0);
-  const outBoxRef = useRef();
-  const [restCardCount, setRestCardCount] = useState(null);
+export default function ItemCarousel({imgUrls, updateLeftFlag, leftFlagValue, updateRightFlag, rightFlagValue}}){
 
-  const handleClickArrowBtn = ({
-    currentTarget
-  }) => {
-    const direction = currentTarget.getAttribute('direction');
-    const outBoxWidth = outBoxRef.current.clientWidth;
-    if (direction === 'RIGHT') return moveRight(outBoxWidth);
-    if (direction === 'LEFT') return moveLeft(outBoxWidth);
-  };
+    const options = {
+        carouselWidth: 200,
+        itemsToShow: 1,
+        duration: 0.5,
+        timing: 'ease-in-out',
+        arrowSize: 20,
+      };
 
-  const moveRight = outBoxWidth => {
-    if (restCardCount === 0) return;
+      const Item = styled.div`
+      margin: 0 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 40px;
+      background: linear-gradient(to right, #00FFFF, #A020F0);
+      border-radius: 10px;
+      width: 190px; // 200px - margin(5px + 5px)
+      height: 200px;
+      `;
+      const items = imgUrls.map ((card) => (
+        <Item><img src={card}></img></Item>
+      ));
 
-    if (restCardCount < itemsToShow) {
-      setRestCardCount(0);
-      return setPosition(position - outBoxWidth / itemsToShow * restCardCount);
-    }
+      return(
+        <div>
+            <Carousel {...options, updateLeftFlag={updateLeftFlag}, leftFlagValue={leftFlagValue}, updateRightFlag={updateRightFlag}, rightFlagValue={rightFlagValue}}>{items}</Carousel>
+        </div>
+      )
 
-    setRestCardCount(cardCount => cardCount - itemsToShow);
-    updateRightFlag(rightFlagValue + 1);
-    setPosition(position - outBoxWidth);
-  };
-
-  const moveLeft = outBoxWidth => {
-    if (position >= 0) return;
-    const defaultCardCount = children.length - itemsToShow;
-
-    if (restCardCount + itemsToShow > defaultCardCount) {
-      setRestCardCount(defaultCardCount);
-      updateFlag(position- outBoxWidth / itemsToShow * restCardCount);
-      return setPosition(0);
-    }
-
-    setRestCardCount(cardCount => cardCount + itemsToShow);
-    setPosition(position + outBoxWidth);
-    updateLeftFlag(leftFlagValue + 1);
-    updateFlag(position);
-  };
-
-  useEffect(() => {
-    if (restCardCount !== null) return;
-    setRestCardCount(children.length - itemsToShow);
-  }, [children, itemsToShow, restCardCount]);
-  return /*#__PURE__*/React.createElement(CarouselStyled, {
-    className: "carousel",
-    carouselWidth: carouselWidth
-  }, /*#__PURE__*/React.createElement(OutBox, {
-    ref: outBoxRef
-  }, /*#__PURE__*/React.createElement(Items, {
-    position: position,
-    duration: duration,
-    timing: timing
-  }, children)), /*#__PURE__*/React.createElement(Arrow, {
-    size: arrowSize,
-    direction: 'RIGHT',
-    onClick: e => handleClickArrowBtn(e)
-  }), /*#__PURE__*/React.createElement(Arrow, {
-    size: arrowSize,
-    direction: 'LEFT',
-    onClick: e => handleClickArrowBtn(e)
-  }));
-};
-
-export default Carousel;
-const CarouselStyled = styled.div`
-  width: ${({
-  carouselWidth
-}) => carouselWidth ? `${carouselWidth}px` : `100%`};
-  position: relative;
-`;
-const OutBox = styled.div`
-  overflow: hidden;
-  position: relative;
-`;
-const Items = styled.div`
-  display: flex;
-  width: fit-content;
-  position: relative;
-  transition: ${({
-  duration,
-  timing
-}) => `all ${duration}s ${timing}`};
-  transform: ${({
-  position
-}) => `translateX(${position}px)`};
-`;
+}
